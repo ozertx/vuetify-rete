e14a21aa18e66f70e7f970738de87b366b9e482e
Vladimir <ozertx@gmail.com>
2022-02-19 01:38:13 +0300

    +schemas

--- src/_app/index.ts
+++ src/_app/index.ts
@@ -3,81 +3,65 @@ import yaml from 'ajv'
 import axios from 'axios'
 import moment from 'moment'
 
-import appConfig from './app-config'
+import appConfigSchema from './schemas/app-config-schema.json'
+import reteBlocksSchema from './schemas/rete-blocks-schema.json'
+import unitConfig from 'cyber-builder/cyber-builder/schemas/unit-config.json'
+import buildConfig from 'cyber-builder/cyber-builder/schemas/build-config.json'
 
 const SCHEMAS: { [keyof: string]: any } = {
-	"app-config": {
-		"type": "object",
-		"required": ["kind", "version", "name", "title", "routes", "units"],
-		"properties": {
-			"kind": { "type": "string" },
-      "version": { "type": "string" },
-      "title": { "type": "string" },
-      "name": { "type": "string" },
-			"routes": {
-				"type": "object"
-			},
-      "units": {
-        "type": "object",
-        "additionalProperties": { "$ref": "#/definitions/ui-unit"}
-      }
-		},
-		"additionalProperties": false,
-    "definitions": {
-      "ui-unit": {
-        "type": "object",
-        "properties": {
-          "name": { "type": "string" },
-        }
-      }
-    }
-	}
+  "app-config": appConfigSchema,
+  "rete-blocks-schema": reteBlocksSchema,
+  "unit-config": unitConfig,
+  "build-config": buildConfig,
 }
+
+console.log(SCHEMAS)
 
 const ajv = new Ajv({ allErrors: true })
-const validate = Object.keys(SCHEMAS).reduce( (acc: any, key) => { acc[key] = ajv.compile(SCHEMAS[key]); return acc } , {} )
-
-const APP: any = {}
-const appState: any = {
-  "status": "active",
-  "error": null
-}
-
-const appUnits: any = {
-  "service": {
-    "icon": "mdi-cog"
-  },
-  "profile": {
-    "icon": "mdi-account"
-
-  },
-  "appLogo": {
-    "icon": "mdi-android"
-  },
-  "layer001": {
-    "icon": "mdi-layers"
-  },
-  "layer002": {
-    "icon": "mdi-layers"
-  },
-}
-
-const loadApp = async () => {
-
-  if(!validate['app-config'](appConfig)) {
-    console.log(`app-config validation errors`,validate['app-config'].errors)
-    appState.error = {
-      error: `invalid App schema`,
-      item: appConfig,
-      errors: validate['app-config'].errors, 
+const validate = Object.keys(SCHEMAS).reduce( 
+  (acc: any, key) => {
+    if (!ajv.validateSchema(SCHEMAS[key])) {
+      console.error(ajv.errors)
+      console.error(`schema ${key} definition ERR`, key)
+      throw new Error(`schema ${key} definition ERR`)
     }
-    return
+    
+    try {
+      acc[key] = ajv.compile(SCHEMAS[key])
+      return acc 
+    }
+    catch(err) {
+      console.error(`schema ${key} compile ERR`)
+      throw err
+    }
+  }, {} )
+  
+  const APP: any = {}
+  const appState: any = {
+    "status": "active",
+    "error": null
   }
-
-  APP.config = appConfig
-  return APP
-}
-
+  
+  import * as appConfig from './app-config.json'
+  import * as appUnits1 from './app-units.json'
+  const appUnits: any = appUnits1
+  
+  const loadApp = async () => {
+    
+    if(!validate['app-config'](appConfig)) {
+      console.log(`app-config validation errors`,validate['app-config'].errors)
+      appState.error = {
+        error: `invalid App schema`,
+        item: appConfig,
+        errors: validate['app-config'].errors, 
+      }
+      return
+    }
+    
+    APP.config = appConfig
+    return APP
+  }
+  
 export {
   yaml, ajv, validate, axios, moment, loadApp, APP, appState, appUnits
 }
